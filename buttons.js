14631576919edf2513805053694e89bf00da7ac4
GabrScarpinella <118476744+GabrScarpinella@users.noreply.github.com>
2022-12-09 15:34:08 -0300

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,5 +1,5 @@
-function main(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/main.html","_self");}
-function life(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/life.html","_self");}
-function creations(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/creations.html","_self");}
-function games(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/games.html","_self");}
-function albums(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/albums.html","_self");}
+function main(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/main.html", "_self");}
+function life(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/life.html", "_self");}
+function creations(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/creations.html", "_self");}
+function games(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/games.html", "_self");}
+function albums(){window.open("https://gabrscarpinella.github.io/GabrScarpinella/albums.html", "_self");}
